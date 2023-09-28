b67d476117abe40b9181ed40552664bec135bacd
ExtraStudent101 <theerapat_lum@cmu.ac.th>
2023-09-28 15:36:02 +0700

    Done

--- src/app/layout.js
+++ src/app/layout.js
@@ -91,8 +91,8 @@ export default function RootLayout({ children }) {
               </Title>
               {children}
               <Footer
-                studentId="650610560"
-                fullName="Chayanin Suatap"
+                studentId="650610772"
+                fullName="Theerapat Lumtan"
                 year="2023"
               />
             </Container>
